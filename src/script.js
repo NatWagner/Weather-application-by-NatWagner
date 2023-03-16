0f5dc580ad4369b46eb0aa1ff35995924fc2636d
NatWagner <124263343+NatWagner@users.noreply.github.com>
2023-03-16 21:17:31 +0100

    Cleaning up code

--- src/script.js
+++ src/script.js
@@ -53,7 +53,7 @@ dateElement.innerHTML = formatDate(currentTime);
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
 
   return days[day];
 }
