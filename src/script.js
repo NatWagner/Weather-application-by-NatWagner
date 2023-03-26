d63ba47a1bd3166d724a4189c484ab9c962e712b
NatWagner <124263343+NatWagner@users.noreply.github.com>
2023-03-26 17:23:53 +0200

    Removed console.log

--- src/script.js
+++ src/script.js
@@ -97,7 +97,6 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "e450bc345a80a08ada69fd5c714d871d";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
